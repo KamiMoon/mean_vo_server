ef01fac10372fbd854f869aede5ef1d34c2ac25f
Eric Kizaki <erickizaki@gmail.com>
2015-09-05 19:06:52 -0500

    Server project
    Based on angular_fullstack

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/meanvoserver-dev'
+  },
+
+  seedDB: true
+};
