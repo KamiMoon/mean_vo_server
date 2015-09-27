d61471d3e70b0694e63f656d478d9b4cecc0c2ce
Eric Kizaki <erickizaki@gmail.com>
2015-09-26 19:55:14 -0500

    Cleanup

--- client/components/bInput/bInput.directive.spec.js
+++ client/components/bInput/bInput.directive.spec.js
@@ -12,11 +12,10 @@ describe('Directive: bInput', function() {
         scope = $rootScope.$new();
     }));
 
-    it('should make hidden element visible', inject(function($compile) {
+    it('should create a default html', inject(function($compile) {
         element = angular.element('<b-input></b-input>');
         element = $compile(element)(scope);
         scope.$apply();
-        //expect(element.text()).toBe('this is the bInput directive');
 
         console.log(element[0].outerHTML);
         expect(element[0].outerHTML).toBe('<b-input class="ng-scope"><div class="form-group ng-scope"><label></label><input type="text" class="form-control"></div></b-input>');
