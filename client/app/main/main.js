32f3b6df7c346853dae4bf56a6106cdb05a05889
Eric Kizaki <erickizaki@gmail.com>
2015-09-27 15:34:48 -0500

    Some static pages

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,11 +1,20 @@
 'use strict';
 
 angular.module('meanVoServerApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('main', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
-  });
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('main', {
+                url: '/',
+                templateUrl: 'app/main/main.html',
+                controller: 'MainCtrl'
+            }).state('about', {
+                url: '/about',
+                templateUrl: 'app/main/about.html'
+            }).state('contact', {
+                url: '/contact',
+                templateUrl: 'app/main/contact.html'
+            }).state('faq', {
+                url: '/faq',
+                templateUrl: 'app/main/faq.html'
+            });
+    });
